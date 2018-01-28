b4e292dbb208d0f9a993fbe7ac4c227607830225
as-is-prog <as-is-prog@outlook.jp>
2018-01-28 14:35:06 +0900

    [add] Socketサーバ他追加

---
+++ notifySocket.rb
@@ -0,0 +1,38 @@
+require 'em-websocket'
+require 'json'
+require 'net/http'
+require 'uri'
+
+connections = []
+
+EM.run {
+  EM::WebSocket.run(:host => "0.0.0.0", :port => 8888) do |ws|
+    ws.onopen {
+      puts "WebSocket connection open"
+    }
+
+    ws.onclose {
+        puts "Connection closed"
+        connections.reject!{|conn| conn[:ws] == ws };
+        puts "conn_count:#{connections.length}"
+    }
+
+    ws.onmessage { |msg|
+        msgObj = JSON.parse(msg);
+        puts msg;
+        puts msgObj["isOpenMsg"]
+        if msgObj["isOpenMsg"] then
+            puts "connect"
+            connections << { ws: ws , employeeId: msgObj["employeeId"] }
+            puts "conn_count:#{connections.length}"
+        elsif msgObj["yabee"] then
+            url = "http://localhost:4567/api/free"
+            res = Net::HTTP.get(URI.parse(url))
+            himas = JSON.parse(res);
+
+            puts "Recieved message: #{msg}"
+            connections.reject{|conn| himas.include?(msgObj["employeeId"]) || conn[:ws] == ws }.each{|conn| conn[:ws].send("#{msgObj["msg"]}さんがマジやべぇです！") }
+        end
+    }
+  end
+}
