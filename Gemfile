12d6cf3d25f9fc6f079e91fce7cb3edd2fed07f8
as-is-prog <as-is-prog@outlook.jp>
2018-01-28 14:32:30 +0900

    socketserver,やべぇテスト用ページ、loginページの追加他

--- Gemfile
+++ Gemfile
@@ -8,3 +8,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gem 'sinatra', '~> 1.4', '>= 1.4.8', :require => true
 gem 'sinatra-contrib', '~> 1.4', '>= 1.4.7', :require => ['sinatra/reloader', 'sinatra/json']
 gem 'mongo', :require => ['bson', 'mongo']
+gem 'em-websocket'
--- app.rb
+++ app.rb
@@ -3,7 +3,57 @@
 require 'bundler'
 Bundler.require
 
-get '/' do
-  erb :index
+require "json"
+
+configure do
+  enable :sessions
 end
 
+get "/" do
+  if session[:employeeId] != nil then
+      redirect "/home"
+  else
+      erb :login
+  end
+end
+
+post "/login" do
+  client = Mongo::Client.new('mongodb://localhost/hat')
+  collection = client[:users]
+  user = collection.find({'employeeId' => params[:employeeId]}).first
+
+  session[:employeeId] = params[:employeeId]
+  session[:name] = user[:name]
+  redirect "/home"
+end
+
+get "/home" do
+  if session[:employeeId] == nil then
+      redirect "/"
+  else
+      @employeeId = session[:employeeId]
+      @name = session[:name]
+      erb :index
+  end
+end
+
+get "/api/free" do
+  client = Mongo::Client.new('mongodb://localhost/hat')
+  collection = client[:users]
+  freeUsers = collection.find({'isFree' => true}).inject([]){|us,u|
+     us << u[:employeeId]
+  }
+  return JSON.generate(freeUsers)
+end
+
+post "/api/free/set" do #isFreeを送って
+  client = Mongo::Client.new('mongodb://localhost/hat')
+  collection = client[:users]
+  isFree = params[:isFree]
+  collection.update_one({'employeeId' => session[:employeeId]}, {'$set' => {'isFree' => isFree}})
+end
+
+get "/logout" do
+  session.clear
+  redirect '/'
+end
