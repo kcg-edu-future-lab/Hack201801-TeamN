df5efa6f96f5e2df8698aec4ee22c02c6368b311
Alucky4423 <a.lucky2510@gmail.com>
2018-01-28 15:12:22 +0900

    [udpate] 備品リスト表示

--- app.rb
+++ app.rb
@@ -8,8 +8,15 @@ get '/' do
 end
 
 get "/cathandList" do
+  @title = '猫の手リスト'
   DB = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'hack')
   @employee = DB[:users].find({ isFree: true }) || []
   erb :cathandList
 end
 
+get '/fixtures' do
+  @title = '備品'
+  DB = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'hack')
+  @fixtures = DB[:fixtures].distinct('type')
+  erb :fixtures
+end
